e334c5be69ed4d85d0520a3d5486502246a50172
Coffee-crocodile <1147347984@qq.com>
2023-04-14 18:03:47 +0800

    feat(all): init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lint-config
+
+集成eslint,stylelint,commitlint
---
+++ test/vueDemo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+  var a = 1
+  console.log(a)
+</script>
+
+<style scoped></style>
